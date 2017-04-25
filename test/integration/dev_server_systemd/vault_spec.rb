3fd8ea8b41327e096042c337866704c16bfb61ae
Marcus Young <myoung34@my.apsu.edu>
2017-04-25 14:15:31 -0500

    Finally, tests pass

--- test/integration/dev_server_systemd/vault_spec.rb
+++ test/integration/dev_server_systemd/vault_spec.rb
@@ -1,6 +1,16 @@
 describe file('/etc/vault/config/server.hcl') do
   it { should be_a_file }
-  its(:content) { should eq("foo") }
+  expected =<<-EOF
+listener "tcp" {
+  address = "0.0.0.0:8200"
+  tls_disable = 0
+
+}
+
+default_lease_ttl="24h"
+max_lease_ttl="24h"
+EOF
+  its(:content) { should eq(expected) }
 end
 
 describe file('/etc/systemd/system/vault.service') do
