96bb6d7e1172019ecdffb6bcb58ca7fb8c47332a
Dafydd Jones <dafydd@techneg.it>
2019-10-04 03:13:52 +0100

    test(inspec): add baseline profile to share tests between dev and prod
      * avoiding use of `describe.one` due to upstream bug inspec/4547
        results in cleaner code
      * avoid repetition of tests between dev and prod server profiles

--- test/integration/dev_server/controls/vault_spec.rb
+++ test/integration/dev_server/controls/vault_spec.rb
@@ -1,49 +1,19 @@
-describe command('/usr/local/bin/vault -version') do
-  its(:exit_status) { should eq 0 }
-  its(:stderr) { should be_empty }
-  its(:stdout) { should match(/^Vault v[0-9\.]+ \('[0-9a-f]+'\)/) }
+if os[:name] == 'amazon' && os[:release] =~ /^20\d\d/
+  log_command = command('cat /var/log/vault.log')
+else
+  log_command = command('journalctl -u vault')
 end
 
-describe.one do
-  describe file('/etc/systemd/system/vault.service') do
-    it { should be_a_file }
-    its(:content) { should_not match(/syslog/) }
-  end
-
-  describe file('/etc/init/vault.conf') do
-    it { should be_a_file }
-  end
-end
-
-describe service('vault') do
-  it { should be_enabled }
-  it { should be_running }
-end
+include_controls 'vault-server-baseline'
 
 describe file("/etc/vault/conf.d/config.json") do
   it { should_not be_a_file }
 end
 
-describe.one do
-  describe command('journalctl -u vault') do
-    its(:exit_status) { should eq 0 }
-    its(:stderr) { should be_empty }
-    its(:stdout) { should match(/WARNING! dev mode is enabled!/) }
-  end
-
-  describe file('/var/log/vault.log') do
-    it { should be_a_file }
-    its(:content) { should match(/WARNING! dev mode is enabled!/) }
-  end
-end
-
-describe port(8200) do
-  it { should be_listening }
-  its('processes') { should include 'vault' }
-end
-
-describe http('http://127.0.0.1:8200/v1/sys/seal-status') do
-  its('status') { should cmp 200 }
+describe log_command do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should be_empty }
+  its(:stdout) { should match(/WARNING! dev mode is enabled!/) }
 end
 
 describe json(content: http('http://127.0.0.1:8200/v1/sys/seal-status').body) do
